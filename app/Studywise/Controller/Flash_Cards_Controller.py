acd3fe1147934d7d2c7c66f217e2eef0806b9448
AbdelRahmanMohamed <abdelrahman0213@gmail.comd>
2024-02-29 18:49:30 +0200

    add functions to user controller

--- app/Studywise/Controller/Flash_Cards_Controller.py
+++ app/Studywise/Controller/Flash_Cards_Controller.py
@@ -6,25 +6,31 @@ import re
 from Constants.Constants import OPENAI_API_KEY, MAX_TOKENS_PER_REQUEST,kUserId,kUserEmail ,kDatejoined ,kFullName 
 
 class FlashcardsController:
-    # openai_api_key = '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'
-    # openai_api_key = openai_api_key
-    # openai.api_key = self.openai_api_key
-    # max_tokens_per_request = 1000 
-    def __init__(self, openai_api_key):
-        self.openai_api_key = openai_api_key
-        openai.api_key = self.openai_api_key
-        self.max_tokens_per_request = 1000  # Safe limit for tokens per request
-    
+    def __init__(self,material_id):
+        openai.api_key = OPENAI_API_KEY
+        MAX_TOKENS_PER_REQUEST = 4096
+        self.material_id=material_id  # Safe limit for tokens per request
 
-    def is_conceptually_relevant(self, question):
+
+    def is_conceptually_relevant(self,question):
+        # Patterns for non-conceptual questions
         non_conceptual_patterns = [
-            r"\bwho\b", r"\bwhen\b", r"\bwhere\b", r"\bpublication\b",
-            r"\bauthor\b", r"\bpublished\b", r"\bbook\b", r"\bISBN\b",
-            r"\bedition\b", r"\bhistory\b", r"\bhistorical\b"
+            r"\bwho\b",  # Who authored, who developed
+            r"\bwhen\b",  # When was it published/developed
+            r"\bwhere\b",  # Where was it published
+            r"\bpublication\b",
+            r"\bauthor\b",
+            r"\bpublished\b",
+            r"\bbook\b",
+            r"\bISBN\b",
+            r"\bedition\b",
+            r"\bhistory\b",
+            r"\bhistorical\b",
+            # Add more patterns as needed
         ]
         return not any(re.search(pattern, question.lower()) for pattern in non_conceptual_patterns)
 
-    def extract_paragraphs_from_pdf(self, pdf_path):
+    def extract_paragraphs_from_pdf(self,pdf_path):
         paragraphs = []
         with pdfplumber.open(pdf_path) as pdf:
             for page in pdf.pages:
@@ -33,14 +39,66 @@ class FlashcardsController:
                     paragraphs.extend(text.split('\n'))
         return paragraphs
 
-    def generate_qa_pairs_with_chatgpt(self, paragraphs):
+    def extract_and_split_text(self,txt_path, max_length=16000):
+        paragraphs = []
+        with open(txt_path, 'r') as file:
+            for paragraph in file.read().split('\n\n'):
+                trimmed_paragraph = paragraph.strip()
+                if len(trimmed_paragraph) > 0:
+                    if len(trimmed_paragraph) > max_length:
+                        segments = self.split_text_into_segments(trimmed_paragraph, max_length)
+                        paragraphs.extend(segments)
+                    else:
+                        paragraphs.append(trimmed_paragraph)
+        return paragraphs
+
+    def split_text_into_segments(self,text, max_length=16000):
+        words = text.split(' ')
+        segments = []
+        current_segment = ''
+
+        for word in words:
+            if len(current_segment) + len(word) < max_length:
+                current_segment += word + ' '
+            else:
+                last_period = current_segment.rfind('.')
+                if last_period != -1 and (len(current_segment) - last_period) < 200:
+                    segment_to_add = current_segment[:last_period + 1]
+                    current_segment = current_segment[last_period + 2:] + word + ' '
+                else:
+                    segment_to_add = current_segment
+                    current_segment = word + ' '
+                segments.append(segment_to_add.strip())
+        
+        if current_segment:
+            segments.append(current_segment.strip())
+
+        return segments
+
+    def generate_qa_pairs_with_chatgpt(self,paragraphs, content_type):
         qa_pairs = []
         batched_paragraphs = []
         current_batch = ""
 
         for paragraph in paragraphs:
+
+            base_prompt = "Generate questions and answers focusing on the technical and conceptual content of this text. "
+
+            transcript_note = "Noting that the text that will be given might contain grammatical or logical mistakes due to speech-to-text inaccuracies, please focus on generating conceptually relevant and clear questions and answers, avoiding ambiguous content. Only generate quesitons and answers relevant to the following text: " 
+            pdf_note = "Avoiding questions about authors, publication dates, or historical development. Do not refer to the material you have been provided with as 'this text' or 'the text', instead, refer to it with the name of the topic at hand, and do not treat the questions & answers as they are exclusive to this text, for example, do not ask about what this text in particular is talking about. You can ask about definitions of things that were explained in the text: "
+
+
+            if content_type.lower() == "pdf":
+                note = pdf_note
+            elif content_type.lower() == "transcript" or content_type.lower() == "video transcript":
+                note = transcript_note
+            elif content_type.lower() == "txt" or content_type.lower() == "text":
+                note = ""
+            else:
+                note = ""
+
             if len(paragraph) > 20 and "http" not in paragraph:
-                if len(current_batch) + len(paragraph) < self.max_tokens_per_request:
+                if len(current_batch) + len(paragraph) < MAX_TOKENS_PER_REQUEST:
                     current_batch += f"{paragraph}\n\n"
                 else:
                     batched_paragraphs.append(current_batch)
@@ -50,10 +108,14 @@ class FlashcardsController:
             batched_paragraphs.append(current_batch)
 
         for batch in batched_paragraphs:
-            prompt = [
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": f"Generate questions and answers focusing on the technical and conceptual content of this text, avoiding questions about authors, publication dates, or historical development: {batch}"}
-            ]
+
+            prompt_content = base_prompt + note
+            system_prompt = {"role": "system", "content": "You are a helpful assistant."}
+            user_prompt = {"role": "user", "content": prompt_content + ": " + batch}
+
+            print("This is the user prompt that will be sent: Prompt Content:y\n" + prompt_content + "\nBatch:\n" + batch)
+
+            prompt = [system_prompt, user_prompt]
             try:
                 response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt)
                 response_text = response.choices[0].message['content'].strip()
@@ -65,6 +127,7 @@ class FlashcardsController:
             except openai.error.RateLimitError:
                 print("Rate limit reached, waiting for 30 seconds...")
                 time.sleep(30)
+                # Retry the request after waiting
                 response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt)
                 response_text = response.choices[0].message['content'].strip()
                 potential_qa_pairs = response_text.split('\n\n')
@@ -72,30 +135,55 @@ class FlashcardsController:
                     question = pair.split('\n')[0]
                     if self.is_conceptually_relevant(question):
                         qa_pairs.append(pair)
-        
+
         return qa_pairs
 
-    def format_flash_cards(self, qa_pairs):
+    def format_flash_cards(self,qa_pairs):
         formatted_cards = []
         for pair in qa_pairs:
-            if pair.count('\n') == 1:
+            if pair.count('\n') == 1:  # Expecting each pair to be two lines: question and answer
                 question, answer = pair.split('\n')
                 formatted_cards.append({'front': question, 'back': answer})
         return formatted_cards
 
-    def save_flash_cards_to_file(self, formatted_cards, filename):
-        with open(filename, 'w') as file:
+    def filenameFromPath(self,filepath):
+        parts = filepath.rsplit('/', 1)
+
+        if len(parts) == 1: # If no '/' was found, return empty string
+            return ""
+
+        filename_parts = parts[-1].rsplit('.', 1)
+
+        if len(filename_parts) == 1:
+            return parts[-1]
+
+        return filename_parts[0] # Return the filename alone (assuming it was between the last '/' and the last '.')
+
+    # Example usage:
+
+    def save_flash_cards_to_file(self,formatted_cards, filepath):
+        with open(filepath, 'w') as file:
             json.dump(formatted_cards, file, indent=4)
 
-    def process_pdf_to_flashcards(self, pdf_path, output_file):
-        paragraphs = self.extract_paragraphs_from_pdf(pdf_path)
-        qa_pairs = self.generate_qa_pairs_with_chatgpt(paragraphs)
-        formatted_cards = self.format_flash_cards(qa_pairs)
-        self.save_flash_cards_to_file(formatted_cards, output_file)
-        print(f"Flash cards saved to {output_file}")
+    def runFlashcards(self,file_path, content_type = ''):
+        content = []
+
+        filename = self.filenameFromPath(file_path)
+        output_path='assets/output_files/flashcards/'+filename+'_flashcards.json' 
+
+        if content_type == '':
+            if file_path.endswith('.pdf'):
+                content =self. extract_paragraphs_from_pdf(file_path)
+                content_type = 'pdf'
+            elif file_path.endswith('.txt'):
+                content = self.extract_and_split_text(file_path) 
+            else:
+                raise ValueError("Unsupported file type. Only .pdf or .txt files are currently accepted.")
+        
+        
     
     async def get_flashcards(self, processed_material_id):
-        material_doc = await self.db.collection('UsersFlashCards').document(kUserId).collection(self.processed_material.processed_material_id).document(self.flashcard_id
+        material_doc = await self.db.collection('UsersFlashCards').document(kUserId).collection(self.material_id).document(self.flashcard_id
                                                                                                                                  ).get()
                                              
         if material_doc.exists:
@@ -127,8 +215,13 @@ class FlashcardsController:
             print(e)
 
 
-openai_api_key = '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'
-flashcards_controller = FlashcardsController(openai_api_key)
-pdf_path = 'StudyWise/flashcards_from_pdf/test.pdf'
-output_file = 'StudyWise/flashcards_from_pdf/JS/flash_cards.json'
-flashcards_controller.process_pdf_to_flashcards(pdf_path, output_file)
+# openai_api_key = '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'
+# flashcards_controller = FlashcardsController(openai_api_key)
+# pdf_path = 'StudyWise/flashcards_from_pdf/test.pdf'
+# output_file = 'StudyWise/flashcards_from_pdf/JS/flash_cards.json'
+# flashcards_controller.process_pdf_to_flashcards(pdf_path, output_file)
+#------------------
+# qa_pairs = generate_qa_pairs_with_chatgpt(content, content_type)
+#         formatted_cards = format_flash_cards(qa_pairs)
+#         save_flash_cards_to_file(formatted_cards, output_path)
+#         print(f"Flash cards saved to {output_path}")
--- app/Studywise/Controller/User_Controller.py
+++ app/Studywise/Controller/User_Controller.py
@@ -1,52 +1,70 @@
 import datetime
-from firebase_admin import firestore
+from firebase_admin import firestore,auth
 from Model.User import UserRepo
 from Constants import Constants
 from Constants.Constants import OPENAI_API_KEY, MAX_TOKENS_PER_REQUEST,kUserId,kUserEmail ,kDatejoined ,kFullName 
 from flask import request,jsonify,render_template
+from firebase_admin import auth
 
 class UserController:
-    def __init__(self):
+    def __init__(self,user:UserRepo):
         self._firestore = firestore.client()
         self.userList = []
         self.userInfo = None
+        self.db=firestore.client()
+        self.user=user
 
-  
-    async def addUserToFirestore(self, UserID):
+    def add_user_to_auth(self, email, password):
         try:
-            kUserId = UserID
-            await self.db.collection('Users').document(kUserId).set({
-                "Full Name": self.fullName,
+            user = auth.create_user(
+                email=email,
+                password=password
+            )
+            print('Successfully created new user:', user.uid)
+            #return user
+        except Exception as e:
+            print('Error creating new user:', e)
+            #return None
+
+    async def add_user_to_firestore(self, user_id, user_info):
+        try:
+            # Check if the user already exists in Firestore
+            doc_ref = self.db.collection('Users').document(user_id)
+            doc = doc_ref.get()
+            if doc.exists:
+                print(f'User with ID {user_id} already exists in Firestore.')
+                return False, 'User already exists in Firestore.'
+
+            # Check if the user already exists in Firebase Authentication
+            try:
+                user_record = auth.get_user_by_email(user_info['Email'])
+                if user_record:
+                    print(f'User with email {user_info["Email"]} already exists in Firebase Authentication.')
+                    return False, 'User already exists in Firebase Authentication.'
+            except auth.AuthError:
+                # This means the user does not exist in Firebase Authentication
+                pass
+
+            # User does not exist in Firestore and Firebase Authentication, proceed to add
+            await doc_ref.set({
+                "Full Name": user_info['Full Name'],
                 "Joined on": datetime.now().strftime("%d-%B-%Y"),
-                "Email": self.email,
-                "Role": "User",
-                "User_Level": 0
+                "Email": user_info['Email'],
+                "Role": user_info.get("Role", "User"),
+                "User_Level": user_info.get("User_Level", 0)
             })
+            self.add_user_to_auth(user_info['Email'], user_info['Password'])
+            return True, 'User added successfully.'
         except Exception as e:
             print(e)
+            return False, str(e)
+
     async def DeleteUserToFirestore(self, UserID):
         try:
             kUserId = UserID
             await self.db.collection('Users').document(kUserId).delete()
         except Exception as e:
             print(e)
-    @classmethod
-    def fromJson(cls, data):
-        return cls(
-            email=data["Email"],
-            fName=data["Full Name"],
-            dateJoined=data["Joined on"],
-            Role=data["Role"],
-            User_Level=data[0],
-            
-        )
+   
 
-    def toJson(self):
-        data = {
-            "Full Name": self.fullName,
-            "Joined on": self.dateJoined,
-            "Email": self.email,
-            "Role": self.Role,
-            "User_Level": self.User_Level
-        }
-        return data
+    
